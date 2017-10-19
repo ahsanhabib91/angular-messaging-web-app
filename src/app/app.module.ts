c548f34acf17bf89408a441264b63e8922cfa4ca
ahsanhabib91 <habibibnakther@gmail.com>
2017-10-19 09:33:08 +0600

    created necessary components, services and interfaces.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InboxComponent } from './components/inbox/inbox.component';
+import { ComposeComponent } from './components/compose/compose.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InboxComponent,
+    ComposeComponent,
+    AuthenticationComponent,
+    MessagesComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule
